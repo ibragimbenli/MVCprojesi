2073cac31fb78038aaa99628c3cd07ef5033f2b7
ibragim <ibragim@outlook.com.tr>
2022-01-12 10:30:54 +0300

    Add project files.

---
+++ MVCprojesi/Controllers/homeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCprojesi.Controllers
+{
+    public class homeController : Controller
+    {
+        // GET: home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [ChildActionOnly]
+        public PartialViewResult Header()
+        {
+            return PartialView();
+        }
+        [ChildActionOnly]
+        public PartialViewResult Footer()
+        {
+            return PartialView();
+        }
+        [ChildActionOnly]
+        public PartialViewResult FooterBottom()
+        {
+            return PartialView();
+        }
+    }
+}
---
+++ MVCprojesi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCprojesi.MvcApplication" Language="C#" %>
